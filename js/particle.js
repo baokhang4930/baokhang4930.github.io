457c78b8f62d7b19872900dbb61cbb57ec2a7715
KKhangDev <88831776+baokhang4930@users.noreply.github.com>
2024-02-28 22:13:08 +0700

    Remake the website

---
+++ js/particle.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    particleground(document.getElementById('particle'), {
+      dotColor: '#306844',
+      lineColor: '#306844'
+    });
+  }, false);
